7fecbb497ca75dbbfe1923fbf96ef8543e895985
Sai Rithvik <sairithvik2002@gmail.com>
2025-09-18 15:49:52 +0530

    usecase (rendering pages)

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,17 @@
-import Header from './components/Header'
-import Content from './components/Content'
-import Total from './components/Total'
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      { name: 'Fundamentals of React', exercises: 10 },
-      { name: 'Using props to pass data', exercises: 7 },
-      { name: 'State of a component', exercises: 14 }
-    ]
-  }
+
+  const [ counter, setCounter ] = useState(0)
+
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
 
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
