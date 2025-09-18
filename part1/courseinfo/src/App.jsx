203bc0646f64dbb7e274cdc05109ae511abb6713
Sai Rithvik <sairithvik2002@gmail.com>
2025-09-18 15:51:50 +0530

    cont.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-
   const [ counter, setCounter ] = useState(0)
-
 
   setTimeout(
     () => setCounter(counter + 1),
     1000
   )
+
+ 
+  console.log('rendering...', counter)
 
   return (
     <div>{counter}</div>
