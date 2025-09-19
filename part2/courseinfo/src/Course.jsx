eba29e616742878cfe74c2cc9797e57cf043a353
Sai Rithvik <sairithvik2002@gmail.com>
2025-09-19 18:41:28 +0530

    exc 2.5

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -14,10 +14,16 @@ const Content = ({ parts }) => (
   </div>
 )
 
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p><strong>Total of {total} exercises</strong></p>
+}
+
 const Course = ({ course }) => (
   <div>
     <Header name={course.name} />
     <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </div>
 )
 
