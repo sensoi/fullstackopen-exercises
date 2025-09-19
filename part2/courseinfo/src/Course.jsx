7468b2527f8e95c4671f9912e7b5a3651aadfaf2
Sai Rithvik <sairithvik2002@gmail.com>
2025-09-19 18:37:30 +0530

    exc 2.1-2.5

---
+++ part2/courseinfo/src/Course.jsx
@@ -0,0 +1,24 @@
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+  </div>
+)
+
+export default Course
