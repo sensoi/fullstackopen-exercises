9b5818902436d6f7e69f805f23a37dc6e15b03b0
Sai Rithvik <sairithvik2002@gmail.com>
2025-12-15 14:03:27 +0530

    exc 4.8

---
+++ part4/blogapp/controllers/blogs.js
@@ -0,0 +1,9 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (req, res) => {
+  const blogs = await Blog.find({})
+  res.json(blogs)
+})
+
+module.exports = blogsRouter
--- part4/blogapp/index.js
+++ part4/blogapp/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
 
 mongoose.set('strictQuery', false)
 
@@ -16,6 +18,8 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
 
 app.get('/', (req, res) => {
   res.send('Blog API running')
